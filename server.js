08ab8a5c828962f50c8c33708befb2a54e66883d
divy98696 <dm27091996@gmail.com>
2020-08-18 12:33:07 +0530

    Project Completed

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -40,6 +43,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Public API
+app.use(cors());
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
